20757a0160d1d7d594ea6f1a30833953b18e47b9
MattasLT <matas@smaragdas.xyz>
2023-10-13 21:28:19 +0300

    Commit to git

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'matttas.'
+description 'Loading screen for fivem in tailwind'
+
+files {
+    'index.html',
+    'img/loading.mp4',
+    'music/calypso.mp3',
+}
+
+loadscreen 'index.html'
